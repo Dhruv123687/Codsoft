aa92a0399a767b0bd519de674efd98e0276501eb
Dhruv <saxenadhruv505@gmail.com>
2023-10-12 22:27:02 +0530

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+
+def on_button_click(value):
+    cur = str(entry.get())
+    entry.delete(0, tk.END)
+    entry.insert(0, cur + value)
+
+def calculate():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(0, str(result))
+    except Exception as e:
+        entry.delete(0, tk.END)
+        entry.insert(0, "Error")
+
+root = tk.Tk()
+root.title("Simple Calculator")
+
+entry = tk.Entry(root, width=16, font=("Arial", 24), borderwidth=2, relief="solid")
+entry.grid(row=0, column=0, columnspan=4)
+
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', 'C', '=', '+'
+]
+
+row_val = 1
+col_val = 0
+
+for button_text in buttons:
+    button = tk.Button(root, text=button_text, padx=20, pady=20, font=("Arial", 18),
+                       command=lambda value=button_text: on_button_click(value) if value != '=' else calculate())
+    button.grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+
+root.mainloop()
